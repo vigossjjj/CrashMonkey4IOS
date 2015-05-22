878fb3a0acf7a6a4c7b800b898ba5b37ecbf3577
vigossjjj <jiangyunpeng@58.com>
2015-05-22 15:38:41 +0800

    update reset.sh, add imagemagick depends install.

--- reset.sh
+++ reset.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
-ruby_mode_erubis=`gem list | grep erubis`
-libimobiledevice=`brew list libimobiledevice 2>&1`
+function gem_installer()
+{
+	gem_name=$1
+	gem_info=`gem list | grep $gem_name`
+	if [ "$gem_info" = "" ]; then
+		echo "gem install ruby library $gem_name..."
+		gem install $gem_name
+		echo "gem install ruby library $gem_name done."
+	else
+		echo "gem already installed ruby library $gem_name."
+	fi
 
-if [ "$ruby_mode_erubis" = "" ]; then
-	echo "install ruby depends module..."
-	gem install erubis
-	echo "install ruby depends module done."
-else
-	echo "ruby depends module already install."
-fi
+}
 
-if [[ "$libimobiledevice" =~ "Error: No such keg: /usr/local/Cellar/libimobiledevice" ]]; then
-	echo "install depends libimobiledevice..."
-	brew update >/dev/null 2>&1
-	wait
-	brew install libimobiledevice
-	wait
-	echo "install depends libimobiledevice done."
-else
-	echo "libimobiledevice already install."
-	echo "upgrade libimobiledevice..."
-	brew upgrade libimobiledevice >/dev/null 2>&1
-	echo "upgrade libimobiledevice done."
-fi 
+function brew_installer()
+{	
+	module_name=$1
+	module_info=`brew list $module_name 2>&1`
+	if [[ "$module_info" =~ "Error: No such keg: /usr/local/Cellar/$module_name" ]]; then
+		echo "install depends $module_name..."
+		brew update >/dev/null 2>&1
+		wait
+		brew install $module_name
+		wait
+		echo "install depends $module_name done."
+	else
+		echo "$module_name already install."
+		echo "upgrade $module_name..."
+		brew upgrade $module_name >/dev/null 2>&1
+		echo "upgrade $module_name done."
+	fi 
+}
+
+# install ruby library via gem
+gem_installer erubis
+
+# install 3rd depends library via brew
+brew_installer libimobiledevice
+brew_installer imagemagick
